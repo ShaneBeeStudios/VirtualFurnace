26c9ba3176d19b90f1230cea02fad3fb421a75c2
ShaneBee <shanebolenback@me.com>
2020-04-01 12:51:16 -0700

    FurnaceCommand - use furnace manager methods

--- src/main/java/tk/shanebee/fakefurnace/command/FurnaceCommand.java
+++ src/main/java/tk/shanebee/fakefurnace/command/FurnaceCommand.java
@@ -2,34 +2,24 @@ package tk.shanebee.fakefurnace.command;
 
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
-import org.bukkit.NamespacedKey;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
-import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.ItemMeta;
-import org.bukkit.persistence.PersistentDataType;
 import org.jetbrains.annotations.NotNull;
 import tk.shanebee.fakefurnace.FakeFurnace;
 import tk.shanebee.fakefurnace.FurnaceManager;
 import tk.shanebee.fakefurnace.machine.Furnace;
-import tk.shanebee.fakefurnace.util.Util;
 
 import java.util.UUID;
 
 public class FurnaceCommand implements CommandExecutor {
 
-    private final FakeFurnace plugin;
     private final FurnaceManager furnaceManager;
-    private final NamespacedKey key;
 
     public FurnaceCommand(FakeFurnace plugin) {
-        this.plugin = plugin;
         this.furnaceManager = plugin.getFurnaceManager();
-        this.key = new NamespacedKey(plugin, "furnace");
     }
 
     @Override
@@ -68,8 +58,7 @@ public class FurnaceCommand implements CommandExecutor {
                         }
                         switch (args[1]) {
                             case "new":
-                                Furnace furnace2 = this.furnaceManager.createFurnace("&3Portable Furnace");
-                                player.getInventory().addItem(getFurnaceItem(furnace2.getUuid()));
+                                player.getInventory().addItem(this.furnaceManager.createItemWithFurnace("&3Portable Furnace", Material.FURNACE, true));
                                 break;
                             case "clone":
                                 ItemStack item = player.getInventory().getItemInMainHand();
@@ -79,9 +68,9 @@ public class FurnaceCommand implements CommandExecutor {
                         break;
                     case "id":
                         ItemStack item = player.getInventory().getItemInMainHand();
-                        UUID uuid = getUUIDFromItem(item);
-                        if (uuid != null) {
-                            player.sendMessage("Portable furnace ID: " + uuid.toString());
+                        Furnace furnace = this.furnaceManager.getFurnaceFromItemStack(item);
+                        if (furnace != null) {
+                            player.sendMessage("Portable furnace ID: " + furnace.getUuid().toString());
                         } else {
                             player.sendMessage("This item has no ID!");
                         }
@@ -90,27 +79,6 @@ public class FurnaceCommand implements CommandExecutor {
 
         }
         return true;
-    }
-
-    private ItemStack getFurnaceItem(UUID uuid) {
-        ItemStack itemStack = new ItemStack(Material.FURNACE);
-        ItemMeta meta = itemStack.getItemMeta();
-        assert meta != null;
-        meta.setDisplayName(Util.getColString("&3Portable Furnace"));
-        meta.addEnchant(Enchantment.ARROW_DAMAGE, 1, true);
-        meta.addItemFlags(ItemFlag.HIDE_ENCHANTS);
-        meta.getPersistentDataContainer().set(this.key, PersistentDataType.STRING, uuid.toString());
-        itemStack.setItemMeta(meta);
-        return itemStack;
-    }
-
-    private UUID getUUIDFromItem(ItemStack item) {
-        ItemMeta meta = item.getItemMeta();
-        if (meta.getPersistentDataContainer().has(this.key, PersistentDataType.STRING)) {
-            String u = meta.getPersistentDataContainer().get(this.key, PersistentDataType.STRING);
-            return UUID.fromString(u);
-        }
-        return null;
     }
 
 }
