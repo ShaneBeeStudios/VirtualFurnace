e5fb78fbb8dc5bee55185e9d0ac46fef6617452f
ShaneBee <shanebolenback@me.com>
2020-04-01 00:27:09 -0700

    Util - simple method for getting a namespaced key for this plugin

--- src/main/java/tk/shanebee/fakefurnace/util/Util.java
+++ src/main/java/tk/shanebee/fakefurnace/util/Util.java
@@ -1,10 +1,17 @@
 package tk.shanebee.fakefurnace.util;
 
 import org.bukkit.ChatColor;
+import org.bukkit.NamespacedKey;
+import tk.shanebee.fakefurnace.FakeFurnace;
 
 public class Util {
 
     public static String getColString(String string) {
         return ChatColor.translateAlternateColorCodes('&', string);
     }
+
+    public static NamespacedKey getKey(String key) {
+        return new NamespacedKey(FakeFurnace.getPlugin(), key);
+    }
+
 }
