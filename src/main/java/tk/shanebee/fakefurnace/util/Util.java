58f39f9eb47e005febb62318142f168a4c7a285a
ShaneBee <shanebolenback@me.com>
2020-04-01 02:09:49 -0700

    Util - add methods for checking Minecraft version

--- src/main/java/tk/shanebee/fakefurnace/util/Util.java
+++ src/main/java/tk/shanebee/fakefurnace/util/Util.java
@@ -1,5 +1,6 @@
 package tk.shanebee.fakefurnace.util;
 
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.NamespacedKey;
 import tk.shanebee.fakefurnace.FakeFurnace;
@@ -12,6 +13,34 @@ public class Util {
 
     public static NamespacedKey getKey(String key) {
         return new NamespacedKey(FakeFurnace.getPlugin(), key);
+    }
+
+    /** Check if server is running a minimum Minecraft version
+     * @param major Major version to check (Most likely just going to be 1)
+     * @param minor Minor version to check
+     * @return True if running this version or higher
+     */
+    public static boolean isRunningMinecraft(int major, int minor) {
+        return isRunningMinecraft(major, minor, 0);
+    }
+
+    /** Check if server is running a minimum Minecraft version
+     * @param major Major version to check (Most likely just going to be 1)
+     * @param minor Minor version to check
+     * @param revision Revision to check
+     * @return True if running this version or higher
+     */
+    public static boolean isRunningMinecraft(int major, int minor, int revision) {
+        String[] version = Bukkit.getServer().getBukkitVersion().split("-")[0].split("\\.");
+        int maj = Integer.parseInt(version[0]);
+        int min = Integer.parseInt(version[1]);
+        int rev;
+        try {
+            rev = Integer.parseInt(version[2]);
+        } catch (Exception ignore) {
+            rev = 0;
+        }
+        return maj >= major && min >= minor && rev >= revision;
     }
 
 }
