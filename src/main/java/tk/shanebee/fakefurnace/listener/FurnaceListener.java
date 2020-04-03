a0239483664580f9d8828146871b3300c76dbc4b
ShaneBee <shanebolenback@me.com>
2020-04-03 04:21:46 -0700

    FurnaceListener - change system for clicking with furnace item

--- src/main/java/tk/shanebee/fakefurnace/listener/FurnaceListener.java
+++ src/main/java/tk/shanebee/fakefurnace/listener/FurnaceListener.java
@@ -1,46 +1,35 @@
 package tk.shanebee.fakefurnace.listener;
 
-import org.bukkit.NamespacedKey;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
-import org.bukkit.inventory.EquipmentSlot;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.ItemMeta;
-import org.bukkit.persistence.PersistentDataType;
 import tk.shanebee.fakefurnace.FakeFurnace;
+import tk.shanebee.fakefurnace.FurnaceManager;
 import tk.shanebee.fakefurnace.machine.Furnace;
-
-import java.util.UUID;
 
 public class FurnaceListener implements Listener {
 
-    private FakeFurnace plugin;
-    private NamespacedKey key;
+    private final FurnaceManager furnaceManager;
 
     public FurnaceListener(FakeFurnace plugin) {
-        this.plugin = plugin;
-        this.key = new NamespacedKey(plugin, "furnaceID");
+        this.furnaceManager = plugin.getFurnaceManager();
     }
 
     @EventHandler
     private void onClickFurnace(PlayerInteractEvent event) {
-        if (event.getHand() != EquipmentSlot.HAND) return;
+        Action action = event.getAction();
+        if (action != Action.RIGHT_CLICK_AIR && action != Action.RIGHT_CLICK_BLOCK) return;
+
         ItemStack hand = event.getItem();
         if (hand == null) return;
 
-        ItemMeta meta = hand.getItemMeta();
-        assert meta != null;
-        if (meta.getDisplayName().contains("Portable")) {
+        Furnace furnace = this.furnaceManager.getFurnaceFromItemStack(hand);
+        if (furnace != null) {
             event.setCancelled(true);
-            if (meta.getPersistentDataContainer().has(this.key, PersistentDataType.STRING)) {
-                String u = meta.getPersistentDataContainer().get(this.key, PersistentDataType.STRING);
-                UUID uuid = UUID.fromString(u);
-                Furnace furnace = this.plugin.getFurnaceManager().getByID(uuid);
-                if (furnace != null) {
-                    furnace.openInventory(event.getPlayer());
-                }
-            }
+            furnace.openInventory(event.getPlayer());
         }
     }
+
 }
