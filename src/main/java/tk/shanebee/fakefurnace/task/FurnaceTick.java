19c5a49239bdc29201e73fe476cc502feee674f3
ShaneBee <shanebolenback@me.com>
2020-04-03 04:20:42 -0700

    FurnaceTick - changes - Renamed -

--- src/main/java/tk/shanebee/fakefurnace/task/FurnaceTick.java
+++ src/main/java/tk/shanebee/fakefurnace/task/FurnaceTick.java
@@ -5,22 +5,25 @@ import tk.shanebee.fakefurnace.FakeFurnace;
 import tk.shanebee.fakefurnace.FurnaceManager;
 import tk.shanebee.fakefurnace.machine.Furnace;
 
-public class FurnaceTimer extends BukkitRunnable {
+/**
+ * Timer for ticking furnaces
+ */
+public class FurnaceTick extends BukkitRunnable {
 
     private final FurnaceManager furnaceManager;
     private int tick;
 
-    public FurnaceTimer(FakeFurnace plugin) {
+    public FurnaceTick(FakeFurnace plugin) {
         this.furnaceManager = plugin.getFurnaceManager();
         this.tick = 0;
-        this.runTaskTimer(plugin, 1, 1);
+        this.runTaskTimerAsynchronously(plugin, 1, 1);
     }
 
     @Override
     public void run() {
-        for (Furnace furnace : this.furnaceManager.getFurnaceMap().values()) {
-            furnace.tick();
-        }
+        try {
+            this.furnaceManager.getAllFurnaces().forEach(Furnace::tick);
+        } catch (Exception ignore) {}
         tick++;
         if (tick >= 6000) {
             this.furnaceManager.saveAll();
