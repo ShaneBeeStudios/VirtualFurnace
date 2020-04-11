5391b33baea5d65f61bd6e733f58e63edfd3e022
ShaneBee <shanebolenback@me.com>
2020-04-11 15:56:07 -0700

    add item builders

---
+++ src/main/java/com/shanebeestudios/vf/api/builder/ItemBuilder.java
@@ -0,0 +1,199 @@
+package com.shanebeestudios.vf.api.builder;
+
+import com.shanebeestudios.vf.api.util.Util;
+import org.bukkit.Material;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.inventory.ItemFlag;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Consumer;
+
+/**
+ * Builder to easily create {@link ItemStack ItemStacks}
+ * <p>Create a new builder, manipulate it's meta,
+ * and finish off using <b>{@link #build()}</b> to get your <b>{@link ItemStack}</b></p>
+ */
+@SuppressWarnings("unused")
+public class ItemBuilder {
+
+    ItemStack itemStack;
+    final ItemMeta itemMeta;
+
+    /**
+     * Start a new ItemBuilder
+     * <p>Amount will default to 1</p>
+     *
+     * @param material Material for this new item
+     */
+    public ItemBuilder(@NotNull Material material) {
+        this(material, 1);
+    }
+
+    /**
+     * Start a new ItemBuilder
+     *
+     * @param material Material for this new item
+     * @param amount   Amount for this item
+     */
+    public ItemBuilder(@NotNull Material material, int amount) {
+        itemStack = new ItemStack(material, amount);
+        itemMeta = itemStack.getItemMeta();
+    }
+
+    /**
+     * Start a new ItemBuilder
+     * <p>You can use the consumer to quickly manipulate the item meta if you choose</p>
+     *
+     * @param material Material for this new item
+     * @param meta     Consumer to manipulate meta
+     */
+    public ItemBuilder(@NotNull Material material, @NotNull Consumer<ItemMeta> meta) {
+        this(material, 1, meta);
+    }
+
+    /**
+     * Start a new ItemBuilder
+     * <p>You can use the consumer to quickly manipulate the item meta if you choose</p>
+     *
+     * @param material Material for this new item
+     * @param amount   Amount for this item
+     * @param meta     Consumer to manipulate meta
+     */
+    public ItemBuilder(@NotNull Material material, int amount, @NotNull Consumer<ItemMeta> meta) {
+        itemStack = new ItemStack(material, amount);
+        itemMeta = itemStack.getItemMeta();
+        meta.accept(itemMeta);
+    }
+
+    /**
+     * Set the name of this item
+     * <p>Will accept color codes</p>
+     *
+     * @param name Name to set
+     * @return This ItemBuilder
+     */
+    public ItemBuilder name(@NotNull String name) {
+        itemMeta.setDisplayName(Util.getColString(name));
+        return this;
+    }
+
+    /**
+     * Add lore to this item
+     * <p>Will stack onto previous lore.
+     * Will accept color codes</p>
+     *
+     * @param lore Lore to add
+     * @return This ItemBuilder
+     */
+    public ItemBuilder addLore(@NotNull String lore) {
+        List<String> lores = itemMeta.getLore();
+        if (lores == null) {
+            lores = new ArrayList<>();
+        }
+        lores.add(Util.getColString(lore));
+        itemMeta.setLore(lores);
+        return this;
+    }
+
+    /**
+     * Set the lore of this item
+     * <p>This will wipe any previous lore, will not accept color codes.
+     * <br>You will need to manage colors yourself, consider using
+     * <b>{@link Util#getColString(String)}</b></p>
+     *
+     * @param lores List of lore to add
+     * @return This ItemBuilder
+     */
+    public ItemBuilder lore(@NotNull List<String> lores) {
+        itemMeta.setLore(lores);
+        return this;
+    }
+
+    /**
+     * Add an enchantment to this item
+     *
+     * @param enchantment Enchantment to add
+     * @param level       Level of enchantment to add
+     * @return This ItemBuilder
+     */
+    public ItemBuilder addEnchant(@NotNull Enchantment enchantment, int level) {
+        itemMeta.addEnchant(enchantment, level, true);
+        return this;
+    }
+
+    /**
+     * Hide the enchantments on this item
+     *
+     * @return This ItemBuilder
+     */
+    public ItemBuilder hideEnchants() {
+        itemMeta.addItemFlags(ItemFlag.HIDE_ENCHANTS);
+        return this;
+    }
+
+    /**
+     * Hide the attributes on this item
+     *
+     * @return This ItemBuilder
+     */
+    public ItemBuilder hideAttributes() {
+        itemMeta.addItemFlags(ItemFlag.HIDE_ATTRIBUTES);
+        return this;
+    }
+
+    /**
+     * Hide the unbreakable flag on this item
+     *
+     * @return This ItemBuilder
+     */
+    public ItemBuilder hideUnbreakable() {
+        itemMeta.addItemFlags(ItemFlag.HIDE_UNBREAKABLE);
+        return this;
+    }
+
+    /**
+     * Hide the potion effects on this item
+     *
+     * @return This ItemBuilder
+     */
+    public ItemBuilder hidePotionEffects() {
+        itemMeta.addItemFlags(ItemFlag.HIDE_POTION_EFFECTS);
+        return this;
+    }
+
+    /**
+     * Set the custom model data this item will have
+     *
+     * @param modelData Custom model data to set
+     * @return This ItemBuilder
+     */
+    public ItemBuilder customModelData(int modelData) {
+        itemMeta.setCustomModelData(modelData);
+        return this;
+    }
+
+    /**
+     * Make this item unbreakable
+     *
+     * @return This ItemBuilder
+     */
+    public ItemBuilder unbreakable() {
+        itemMeta.setUnbreakable(true);
+        return this;
+    }
+
+    /**
+     * Get the ItemStack built by this builder
+     *
+     * @return ItemStack built by this builder
+     */
+    public ItemStack build() {
+        itemStack.setItemMeta(itemMeta);
+        return itemStack;
+    }
+
+}
---
+++ src/main/java/com/shanebeestudios/vf/api/builder/PotionItemBuilder.java
@@ -0,0 +1,234 @@
+package com.shanebeestudios.vf.api.builder;
+
+import org.bukkit.Color;
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.inventory.meta.PotionMeta;
+import org.bukkit.potion.PotionData;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
+import org.bukkit.potion.PotionType;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.function.Consumer;
+
+/**
+ * Builder to easily create potion {@link ItemStack ItemStacks}
+ * <p>Create a new builder, manipulate it's meta,
+ * and finish off using <b>{@link #build()}</b> to get your <b>{@link ItemStack}</b></p>
+ */
+@SuppressWarnings("unused")
+public class PotionItemBuilder extends ItemBuilder {
+
+    private PotionMeta potionMeta;
+
+    /**
+     * Start a new ItemBuilder
+     * <p>Material will default to a <b>{@link Material#POTION}</b>. Amount will default to 1</p>
+     */
+    public PotionItemBuilder() {
+        super(Material.POTION, 1);
+    }
+
+    /**
+     * Start a new ItemBuilder
+     * <p>Material must be of the potion variant. Amount will default to 1</p>
+     *
+     * @param material Material for this new item
+     */
+    public PotionItemBuilder(@NotNull Material material) {
+        super(material);
+        if (isNotPotion(material)) {
+            super.itemStack = new ItemStack(Material.POTION, 1);
+        }
+    }
+
+    /**
+     * Start a new ItemBuilder
+     * <p>Material must be of the potion variant.</p>
+     *
+     * @param material Material for this new item
+     * @param amount   Amount for this item
+     */
+    public PotionItemBuilder(@NotNull Material material, int amount) {
+        super(material, amount);
+        if (isNotPotion(material)) {
+            super.itemStack = new ItemStack(Material.POTION, amount);
+        }
+    }
+
+    /**
+     * Start a new ItemBuilder
+     * <p>Material must be of the potion variant.
+     * You can use the consumer to quickly manipulate the item meta if you choose</p>
+     *
+     * @param material Material for this new item
+     * @param meta     Consumer to manipulate meta
+     */
+    public PotionItemBuilder(@NotNull Material material, @NotNull Consumer<ItemMeta> meta) {
+        super(material, meta);
+        if (isNotPotion(material)) {
+            super.itemStack = new ItemStack(Material.POTION);
+        }
+    }
+
+    /**
+     * Start a new ItemBuilder
+     * <p>Material must be of the potion variant.
+     * You can use the consumer to quickly manipulate the item meta if you choose</p>
+     *
+     * @param material Material for this new item
+     * @param amount   Amount for this item
+     * @param meta     Consumer to manipulate meta
+     */
+    public PotionItemBuilder(@NotNull Material material, int amount, @NotNull Consumer<ItemMeta> meta) {
+        super(material, amount, meta);
+        if (isNotPotion(material)) {
+            super.itemStack = new ItemStack(Material.POTION, 1);
+        }
+    }
+
+    /**
+     * Add a {@link PotionEffect} to this item
+     *
+     * @param potionEffect PotionEffect to add
+     * @return This PotionItemBuilder
+     */
+    public PotionItemBuilder addPotion(@NotNull PotionEffect potionEffect) {
+        if (itemMeta instanceof PotionMeta) {
+            ((PotionMeta) itemMeta).addCustomEffect(potionEffect, false);
+        }
+        return this;
+    }
+
+    /**
+     * Set the base potion of this item
+     *
+     * @param type     Base potion type
+     * @param extended whether the potion is extended, <b>{@link PotionType#isExtendable()}</b> must be true
+     * @param upgraded whether the potion is upgraded, <b>{@link PotionType#isUpgradeable()}</b> must be true
+     * @return This PotionItemBuilder
+     */
+    public PotionItemBuilder basePotion(@NotNull PotionType type, boolean extended, boolean upgraded) {
+        if (itemMeta instanceof PotionMeta) {
+            ((PotionMeta) itemMeta).setBasePotionData(new PotionData(type, extended, upgraded));
+        }
+        return this;
+    }
+
+    /**
+     * Add a new potion effect to this item
+     * <p>Icon and particles will default to true,
+     * ambient will default to false, will not have an amplifier</p>
+     *
+     * @param type     Type of effect
+     * @param duration Duration of effect (in ticks)
+     * @return This PotionItemBuilder
+     */
+    public PotionItemBuilder addPotion(@NotNull PotionEffectType type, int duration) {
+        if (itemMeta instanceof PotionMeta) {
+            PotionEffect potionEffect = new PotionEffect(type, duration, 0, false, true, true);
+            ((PotionMeta) itemMeta).addCustomEffect(potionEffect, false);
+        }
+        return this;
+    }
+
+    /**
+     * Add a new potion effect to this item
+     * <p>Icon and particles will default to true,
+     * ambient will default to false</p>
+     *
+     * @param type      Type of effect
+     * @param duration  Duration of effect (in ticks)
+     * @param amplifier Amplifier for this effect
+     * @return This PotionItemBuilder
+     */
+    public PotionItemBuilder addPotion(@NotNull PotionEffectType type, int duration, int amplifier) {
+        if (itemMeta instanceof PotionMeta) {
+            PotionEffect potionEffect = new PotionEffect(type, duration, amplifier, false, true, true);
+            ((PotionMeta) itemMeta).addCustomEffect(potionEffect, false);
+        }
+        return this;
+    }
+
+    /**
+     * Add a new potion effect to this item
+     * <p>Icon and particles will default to true</p>
+     *
+     * @param type      Type of effect
+     * @param duration  Duration of effect (in ticks)
+     * @param amplifier Amplifier for this effect
+     * @param ambient   Whether the effect is ambient or not
+     * @return This PotionItemBuilder
+     */
+    public PotionItemBuilder addPotion(@NotNull PotionEffectType type, int duration, int amplifier, boolean ambient) {
+        if (itemMeta instanceof PotionMeta) {
+            PotionEffect potionEffect = new PotionEffect(type, duration, amplifier, ambient, true, true);
+            ((PotionMeta) itemMeta).addCustomEffect(potionEffect, false);
+        }
+        return this;
+    }
+
+    /**
+     * Add a new potion effect to this item
+     * <p>Icon will default to true</p>
+     *
+     * @param type      Type of effect
+     * @param duration  Duration of effect (in ticks)
+     * @param amplifier Amplifier for this effect
+     * @param ambient   Whether the effect is ambient or not
+     * @param particles Whether the effect will show particles
+     * @return This PotionItemBuilder
+     */
+    public PotionItemBuilder addPotion(@NotNull PotionEffectType type, int duration, int amplifier, boolean ambient, boolean particles) {
+        if (itemMeta instanceof PotionMeta) {
+            PotionEffect potionEffect = new PotionEffect(type, duration, amplifier, ambient, particles, true);
+            ((PotionMeta) itemMeta).addCustomEffect(potionEffect, false);
+        }
+        return this;
+    }
+
+    /**
+     * Add a new potion effect to this item
+     *
+     * @param type      Type of effect
+     * @param duration  Duration of effect (in ticks)
+     * @param amplifier Amplifier for this effect
+     * @param ambient   Whether the effect is ambient or not
+     * @param particles Whether the effect will show particles
+     * @param icon      Whether the effect's icon will show
+     * @return This PotionItemBuilder
+     */
+    public PotionItemBuilder addPotion(@NotNull PotionEffectType type, int duration, int amplifier, boolean ambient, boolean particles, boolean icon) {
+        if (itemMeta instanceof PotionMeta) {
+            PotionEffect potionEffect = new PotionEffect(type, duration, amplifier, ambient, particles, icon);
+            ((PotionMeta) itemMeta).addCustomEffect(potionEffect, false);
+        }
+        return this;
+    }
+
+    /**
+     * Set the color of this item
+     *
+     * @param color Color to set
+     * @return This PotionItemBuilder
+     */
+    public PotionItemBuilder color(@NotNull Color color) {
+        if (itemMeta instanceof PotionMeta) {
+            ((PotionMeta) itemMeta).setColor(color);
+        }
+        return this;
+    }
+
+    private boolean isNotPotion(@NotNull Material material) {
+        switch (material) {
+            case POTION:
+            case LINGERING_POTION:
+            case SPLASH_POTION:
+                return false;
+        }
+        return true;
+    }
+
+}
