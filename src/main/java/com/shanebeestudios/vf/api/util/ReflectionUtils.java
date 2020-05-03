026d667fab0355e6c0339855047319a5e97624e2
ShaneBeee <shanebolenback@me.com>
2020-05-02 18:37:44 -0700

    ReflectionUtils - new

---
+++ src/main/java/com/shanebeestudios/vf/api/util/ReflectionUtils.java
@@ -0,0 +1,121 @@
+package com.shanebeestudios.vf.api.util;
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+@SuppressWarnings("unused")
+public class ReflectionUtils {
+
+    private static final String VERSION = Bukkit.getServer().getClass().getPackage().getName().replace(".", ",").split(",")[3] + ".";
+
+    /**
+     * Check if a method exists
+     *
+     * @param c              Class that contains this method
+     * @param methodName     Method to check
+     * @param parameterTypes Parameter types if the method contains any
+     * @return True if this method exists
+     */
+    public static boolean methodExists(final Class<?> c, final String methodName, final Class<?>... parameterTypes) {
+        try {
+            c.getDeclaredMethod(methodName, parameterTypes);
+            return true;
+        } catch (final NoSuchMethodException | SecurityException e) {
+            return false;
+        }
+    }
+
+    /**
+     * Check if a class exists
+     *
+     * @param className Class to check for existence
+     * @return True if this class exists
+     */
+    public static boolean classExists(final String className) {
+        try {
+            Class.forName(className);
+            return true;
+        } catch (final ClassNotFoundException e) {
+            return false;
+        }
+    }
+
+    public static Class<?> getOBCClass(String obcClassString) {
+        String name = "org.bukkit.craftbukkit." + VERSION + obcClassString;
+        try {
+            return Class.forName(name);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public static Class<?> getNMSClass(String nmsClassString) {
+        String name = "net.minecraft.server." + VERSION + nmsClassString;
+        try {
+            return Class.forName(name);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public static Object getNMSHandle(Entity entity) {
+        try {
+            Method getHandle = entity.getClass().getMethod("getHandle");
+            return getHandle.invoke(entity);
+        } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public static Object getField(String field, Class<?> clazz, Object object) {
+        try {
+            Field f = clazz.getDeclaredField(field);
+            f.setAccessible(true);
+            return f.get(object);
+        } catch (IllegalAccessException | NoSuchFieldException ex) {
+            ex.printStackTrace();
+            return null;
+        }
+    }
+
+    public static void setField(String field, Class<?> clazz, Object object, Object toSet) {
+        try {
+            Field f = clazz.getDeclaredField(field);
+            f.setAccessible(true);
+            f.set(object, toSet);
+        } catch (IllegalAccessException | IllegalArgumentException | NoSuchFieldException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public static void setField(String field, Object object, Object toSet) {
+        try {
+            Field f = object.getClass().getDeclaredField(field);
+            f.setAccessible(true);
+            f.set(object, toSet);
+        } catch (IllegalAccessException | IllegalArgumentException | NoSuchFieldException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public static Object getConnection(Player player)
+            throws SecurityException, NoSuchMethodException, NoSuchFieldException, IllegalArgumentException, IllegalAccessException, InvocationTargetException {
+        Field conField = getHandle(player).getClass().getField("playerConnection");
+        return conField.get(getHandle(player));
+    }
+
+    public static Object getHandle(Player player)
+            throws SecurityException, NoSuchMethodException, IllegalArgumentException, IllegalAccessException, InvocationTargetException {
+        Method getHandle = player.getClass().getMethod("getHandle");
+        return getHandle.invoke(player);
+    }
+
+}
