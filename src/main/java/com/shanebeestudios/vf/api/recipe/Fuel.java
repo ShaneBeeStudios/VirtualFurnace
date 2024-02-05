72c9239ebf84e2a8f68d1d7f5d5bbee666fad5ae
ShaneBee <shanebolenback@me.com>
2024-02-04 16:59:55 -0800

    Fuel - update fuels to match minecraft - scaffolding time was dropped - more fuels were added - re-ordered to match Minecraft (easier for me to find stuff)

--- src/main/java/com/shanebeestudios/vf/api/recipe/Fuel.java
+++ src/main/java/com/shanebeestudios/vf/api/recipe/Fuel.java
@@ -23,82 +23,72 @@ public class Fuel implements Keyed {
 
     public static final Fuel LAVA_BUCKET = get("lava_bucket", Material.LAVA_BUCKET, 20000);
     public static final Fuel BLOCK_OF_COAL = get("block_of_coal", Material.COAL_BLOCK, 16000);
-    public static final Fuel DRIED_KELP_BLOCK = get("dried_kelp_block", Material.DRIED_KELP_BLOCK, 4000);
     public static final Fuel BLAZE_ROD = get("blaze_rod", Material.BLAZE_ROD, 2400);
     public static final Fuel COAL = get("coal", Material.COAL, 1600);
     public static final Fuel CHARCOAL = get("charcoal", Material.CHARCOAL, 1600);
-    public static final Fuel ANY_BOAT = get("any_boat", Tag.ITEMS_BOATS, 1200);
-    public static final Fuel ANY_LOG = get("any_log", Tag.LOGS, 300);
-    public static final Fuel ANY_PLANK = get("any_plank", Tag.PLANKS, 300);
-    public static final Fuel ANY_WOOD_PRESSURE_PLATE = get("any_wood_pressure_plate", Tag.WOODEN_PRESSURE_PLATES, 300);
-    public static final Fuel ANY_WOOD_STAIR = get("any_wood_stair", Tag.WOODEN_STAIRS, 300);
-    public static final Fuel ANY_WOOD_TRAPDOOR = get("any_wood_trapdoor", Tag.WOODEN_TRAPDOORS, 300);
-    public static final Fuel CRAFTING_TABLE = get("crafting_table", Material.CRAFTING_TABLE, 300);
+    public static final Fuel LOGS = get("logs", Tag.LOGS, 300);
+    public static final Fuel PLANKS = get("planks", Tag.PLANKS, 300);
+    public static final Fuel WOODEN_STAIRS = get("wooden_stairs", Tag.WOODEN_STAIRS, 300);
+    public static final Fuel WOODEN_SLABS = get("wooden_slabs", Tag.WOODEN_SLABS, 150);
+    public static final Fuel WOODEN_TRAPDOORS = get("wooden_trapdoors", Tag.WOODEN_TRAPDOORS, 300);
+    public static final Fuel WOODEN_PRESSURE_PLATES = get("wooden_pressure_plates", Tag.WOODEN_PRESSURE_PLATES, 300);
+    public static final Fuel WOODEN_FENCES = get("wooden_fences", Tag.WOODEN_FENCES, 300);
+    public static final Fuel FENCE_GATES = get("fence_gates", Tag.FENCE_GATES, 300);
+    public static final Fuel NOTE_BLOCK = get("note_block", Material.NOTE_BLOCK, 300);
     public static final Fuel BOOKSHELF = get("bookshelf", Material.BOOKSHELF, 300);
+    public static final Fuel CHISELED_BOOKSHELF;
+    public static final Fuel LECTERN = get("lectern", Material.LECTERN, 300);
+    public static final Fuel JUKEBOX = get("jukebox", Material.JUKEBOX, 300);
     public static final Fuel CHEST = get("chest", Material.CHEST, 300);
     public static final Fuel TRAPPED_CHEST = get("trapped_chest", Material.TRAPPED_CHEST, 300);
+    public static final Fuel CRAFTING_TABLE = get("crafting_table", Material.CRAFTING_TABLE, 300);
     public static final Fuel DAYLIGHT_DETECTOR = get("daylight_detector", Material.DAYLIGHT_DETECTOR, 300);
-    public static final Fuel JUKEBOX = get("jukebox", Material.JUKEBOX, 300);
-    public static final Fuel NOTE_BLOCK = get("note_block", Material.NOTE_BLOCK, 300);
-    public static final Fuel MUSHROOM_STEM = get("mushroom_stem", Material.MUSHROOM_STEM, 300);
-    public static final Fuel BROWN_MUSHROOM_BLOCK = get("brown_mushroom_block", Material.BROWN_MUSHROOM_BLOCK, 300);
-    public static final Fuel RED_MUSHROOM_BLOCK = get("red_mushroom_block", Material.RED_MUSHROOM_BLOCK, 300);
-    public static final Fuel ANY_BANNER = get("any_banner", Tag.BANNERS, 300);
-    public static final Fuel ANY_WOODEN_SLAB = get("any_wooden_slab", Tag.WOODEN_SLABS, 150);
+    public static final Fuel BANNERS = get("banners", Tag.BANNERS, 300);
     public static final Fuel BOW = get("bow", Material.BOW, 300);
     public static final Fuel FISHING_ROD = get("fishing_rod", Material.FISHING_ROD, 300);
     public static final Fuel LADDER = get("ladder", Material.LADDER, 300);
-    public static final Fuel ANY_WOODEN_BUTTON = get("any_wooden_button", Tag.WOODEN_BUTTONS, 100);
-    public static final Fuel WOODEN_PICKAXE = get("wooden_pickaxe", Material.WOODEN_PICKAXE, 200);
+    public static final Fuel SIGNS = get("signs", Tag.SIGNS, 200);
+    public static final Fuel HANGING_SIGNS;
     public static final Fuel WOODEN_SHOVEL = get("wooden_shovel", Material.WOODEN_SHOVEL, 200);
+    public static final Fuel WOODEN_SWORD = get("wooden_sword", Material.WOODEN_SWORD, 200);
     public static final Fuel WOODEN_HOE = get("wooden_hoe", Material.WOODEN_HOE, 200);
     public static final Fuel WOODEN_AXE = get("wooden_axe", Material.WOODEN_AXE, 200);
-    public static final Fuel WOODEN_SWORD = get("wooden_sword", Material.WOODEN_SWORD, 200);
-    public static final Fuel ANY_WOODEN_DOOR = get("any_wooden_door", Tag.WOODEN_TRAPDOORS, 200);
-    public static final Fuel BOWL = get("bowl", Material.BOWL, 100);
-    public static final Fuel ANY_SAPLING = get("any_sapling", Tag.SAPLINGS, 100);
+    public static final Fuel WOODEN_PICKAXE = get("wooden_pickaxe", Material.WOODEN_PICKAXE, 200);
+    public static final Fuel WOODEN_DOORS = get("wooden_doors", Tag.WOODEN_DOORS, 200);
+    public static final Fuel BOATS = get("boats", Tag.ITEMS_BOATS, 1200);
+    public static final Fuel WOOL = get("wool", Tag.WOOL, 100);
+    public static final Fuel WOODEN_BUTTONS = get("wooden_buttons", Tag.WOODEN_BUTTONS, 100);
     public static final Fuel STICK = get("stick", Material.STICK, 100);
-    public static final Fuel ANY_WOOL = get("any_wool", Tag.WOOL, 100);
-    public static final Fuel ANY_CARPET = get("any_carpet", Tag.CARPETS, 67);
-
-    // 1.14+
-    public static final Fuel SCAFFOLDING;
-    public static final Fuel CARTOGRAPHY_TABLE;
-    public static final Fuel FLETCHING_TABLE;
-    public static final Fuel SMITHING_TABLE;
-    public static final Fuel LECTERN;
-    public static final Fuel COMPOSTER;
-    public static final Fuel BARREL;
-    public static final Fuel LOOM;
-    public static final Fuel ANY_SIGN;
-    public static final Fuel BAMBOO;
-    public static final Fuel ANY_WOOD_FENCE;
+    public static final Fuel SAPLINGS = get("saplings", Tag.SAPLINGS, 100);
+    public static final Fuel BOWL = get("bowl", Material.BOWL, 100);
+    public static final Fuel WOOL_CARPETS = get("wool_carpets", Tag.WOOL_CARPETS, 67);
+    public static final Fuel DRIED_KELP_BLOCK = get("dried_kelp_block", Material.DRIED_KELP_BLOCK, 4001);
+    public static final Fuel CROSSBOW = get("crossbow", Material.CROSSBOW, 300);
+    public static final Fuel BAMBOO = get("bamboo", Material.BAMBOO, 50);
+    public static final Fuel DEAD_BUSH = get("dead_bush", Material.DEAD_BUSH, 100);
+    public static final Fuel SCAFFOLDING = get("scaffolding", Material.SCAFFOLDING, 50);
+    public static final Fuel LOOM = get("loom", Material.LOOM, 300);
+    public static final Fuel BARREL = get("barrel", Material.BARREL, 300);
+    public static final Fuel CARTOGRAPHY_TABLE = get("cartography_table", Material.CARTOGRAPHY_TABLE, 300);
+    public static final Fuel FLETCHING_TABLE = get("fletching_table", Material.FLETCHING_TABLE, 300);
+    public static final Fuel SMITHING_TABLE = get("smithing_table", Material.SMITHING_TABLE, 300);
+    public static final Fuel COMPOSTER = get("composter", Material.COMPOSTER, 300);
+    public static final Fuel AZALEA = get("azalea", Material.AZALEA, 100);
+    public static final Fuel FLOWERING_AZALEA = get("flowering_azalea", Material.FLOWERING_AZALEA, 100);
+    public static final Fuel MANGROVE_ROOTS;
 
     static {
-        if (Util.isRunningMinecraft(1, 14)) {
-            SCAFFOLDING = get("scaffolding", Material.SCAFFOLDING, 400);
-            CARTOGRAPHY_TABLE = get("cartography_table", Material.CARTOGRAPHY_TABLE, 300);
-            FLETCHING_TABLE = get("fletching_table", Material.FLETCHING_TABLE, 300);
-            SMITHING_TABLE = get("smithing_table", Material.SMITHING_TABLE, 300);
-            LECTERN = get("lectern", Material.LECTERN, 300);
-            COMPOSTER = get("composter", Material.COMPOSTER, 300);
-            BARREL = get("barrel", Material.BARREL, 300);
-            LOOM = get("loom", Material.LOOM, 300);
-            ANY_SIGN = get("any_sign", Tag.SIGNS, 200);
-            BAMBOO = get("bamboo", Material.BAMBOO, 50);
-            ANY_WOOD_FENCE = get("any_wood_fence", Tag.WOODEN_FENCES, 300);
+        if (Util.isRunningMinecraft(1, 19)) {
+            MANGROVE_ROOTS = get("mangrove_roots", Material.MANGROVE_ROOTS, 300);
         } else {
-            SCAFFOLDING = null;
-            CARTOGRAPHY_TABLE = null;
-            FLETCHING_TABLE = null;
-            SMITHING_TABLE = null;
-            LECTERN = null;
-            COMPOSTER = null;
-            BARREL = null;
-            LOOM = null;
-            BAMBOO = null;
-            ANY_SIGN = get("any_sign", Material.getMaterial("SIGN"), 200);
-            ANY_WOOD_FENCE = get("any_wood_fence", Material.OAK_FENCE, 300);
+            MANGROVE_ROOTS = null;
+        }
+        if (Util.isRunningMinecraft(1,20)) {
+            CHISELED_BOOKSHELF = get("chiseled_bookshelf", Material.CHISELED_BOOKSHELF, 300);
+            HANGING_SIGNS = get("hanging_signs", Tag.ITEMS_HANGING_SIGNS, 800);
+        } else {
+            CHISELED_BOOKSHELF = null;
+            HANGING_SIGNS = null;
         }
     }
 
